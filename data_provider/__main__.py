868e7bca283bb414296be9d57ec943183027d55a
Jérôme Picault <jerome.picault@rte-france.com>
2023-06-23 17:19:27 +0200

    bug fixes

--- data_provider/__main__.py
+++ data_provider/__main__.py
@@ -1,26 +1,29 @@
 import typer
 
 from data_provider.bing_news_provider import BingNewsProvider
-
+from data_provider.google_news_provider import GoogleNewsProvider
 
 if __name__ == "__main__":
-    provider = BingNewsProvider()
-
     app = typer.Typer()
 
     @app.command("scrape")
     def scrape(keywords: str = typer.Argument(None, help="keywords for news search engine."),
+               provider: str = typer.Option("bing", help="source for news [bing, google]"),
                save_path: str = typer.Option(None, help="Path for writing results. File is in jsonl format.")
                ):
+        if provider == "bing":
+            provider = BingNewsProvider()
+        else:
+            provider = GoogleNewsProvider()
         results = provider.get_articles([keywords])
         provider.store_articles(results, save_path)
 
     @app.command("auto-scrape")
     def auto_scrape(
-        inputfile_path: str = typer.Argument(
-            None, help="path of jsonlines input file containing the urls."
+        requests: str = typer.Argument(
+            None, help="path of jsonlines input file containing the expected queries."
         ),
-        save_path: str = typer.Argument(None, help="Path for writting results."),
+        save_path: str = typer.Argument(None, help="Path for writing results."),
     ):
         print("Not implemented yet :-)")
 
--- data_provider/google_news_provider.py
+++ data_provider/google_news_provider.py
@@ -1,3 +1,4 @@
+import base64
 from typing import List, Dict, Optional
 from loguru import logger
 import dateparser
@@ -8,16 +9,15 @@ from data_provider.data_provider import DataProvider
 from data_provider.utils import wait
 
 PATTERN = "{QUERY}"
+MAX_ARTICLES = 100
 
-
-class BingNewsProvider(DataProvider):
+class GoogleNewsProvider(DataProvider):
     """News provider for Bing News.
     Limitations:
         - since of results limited to 12
         - hard to request specific dates
     """
-
-    URL_ENDPOINT = f"https://www.bing.com/news/search?q={PATTERN}&format=rss&setLang=fr&sortBy=Date"
+    URL_ENDPOINT = f"https://news.google.com/rss/search?num={MAX_ARTICLES}&hl=fr&gl=FR&ceid=FR:fr&q={PATTERN}"
 
     def __init__(self):
         super().__init__()
@@ -25,7 +25,7 @@ class BingNewsProvider(DataProvider):
     def get_articles(self, keywords: List[str]) -> List[Dict]:
         """Requests the news data provider, collects a set of URLs to be parsed, return results as json lines"""
         query = self._build_query(keywords)
-        logger.debug(f"Querying Bing: {query}")
+        logger.debug(f"Querying Google: {query}")
         result = feedparser.parse(query)
         logger.debug(f"Returned: {len(result['entries'])} entries")
 
@@ -36,15 +36,21 @@ class BingNewsProvider(DataProvider):
         kw = urllib.parse.quote(" ".join(keywords))
         return self.URL_ENDPOINT.replace(PATTERN, f"{kw}")
 
-    def _clean_url(self, bing_url) -> str:
+    def _clean_url(self, url) -> str:
         """Clean encoded URLs returned by Bing news such as "http://www.bing.com/news/apiclick.aspx?ref=FexRss&amp;aid=&amp;tid=649475a6257945d6900378c8310bcfde&amp;url=https%3a%2f%2fwww.lemondeinformatique.fr%2factualites%2flire-avec-schema-gpt-translator-datastax-automatise-la-creation-de-pipelines-de-donnees-90737.html&amp;c=15009376565431680830&amp;mkt=fr-fr"
         """
         try:
-            clean_url = bing_url.split("url=")[1].split("&  ")[0]
-            return urllib.parse.unquote(clean_url)
+            base64_url = url.replace("https://news.google.com/rss/articles/", "").split("?")[0]
+            # fix padding
+            base64_url += "=" * ((4 - len(base64_url) % 4) % 4)
+            #actual_url = base64.b64decode(base64_url)[5:-3].decode('utf-8')
+            actual_url = base64.b64decode(base64_url)[4:-3].decode("ISO-8859-1")
+            if actual_url.startswith("\x01"): # workaround
+                actual_url = actual_url.split("\x01")[1]
+            return actual_url
         except IndexError:
             # fallback (the URL does not match the expected pattern)
-            return bing_url
+            return url
 
     def _get_text(self, url: str) -> str:
         """Extracts text from an article URL"""
@@ -59,9 +65,10 @@ class BingNewsProvider(DataProvider):
             title = entry["title"]
             link = entry["link"]
             url = self._clean_url(link)
+            print(url)
             summary = entry["summary"]
             published = dateparser.parse(entry["published"]).strftime("%Y-%m-%d %H:%M:%S")
-            text = self._get_text(link)
+            text = self._get_text(url)
             return {'title': title, 'summary': summary, 'link': link, 'url': url, 'text': text,
                     'timestamp': published}
         except Exception as e:
