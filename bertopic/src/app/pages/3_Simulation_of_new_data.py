0071f1d0171f7667b0759524da5c2825f38f7746
Jérôme Picault <jerome.picault@rte-france.com>
2023-08-30 16:28:02 +0200

    Refacto simulation new data - separation computation per batch and exploitation of those results, for ex. new visualizations

--- bertopic/src/app/pages/3_Simulation_of_new_data.py
+++ bertopic/src/app/pages/3_Simulation_of_new_data.py
@@ -1,9 +1,11 @@
 from collections import Counter
+from typing import List, Tuple, Union, Dict, Any
 
 import numpy as np
 import pandas as pd
 import streamlit as st
 from metrics import TopicMetrics, TIME_WEIGHT, TEM_x, TEM_y
+from pandas import DataFrame
 from state_utils import register_widget, save_widget_state, restore_widget_state
 from utils import TIMESTAMP_COLUMN, TEXT_COLUMN
 
@@ -61,19 +63,23 @@ def main():
     st.slider("Time weight", min_value=0.0, max_value=0.1, step=0.005, key="tw", on_change=save_widget_state)
 
     if st.button("Run", type="primary"):
-        process_new_data()
+        # computes predictions and related data per batch
+        batch_results = process_new_data_per_batch()
 
-def process_new_data():
-    # Enriches topics with new data
+        # use this data for various display
+        # - plot animated topic map
+        plot_animated_topic_map(batch_results)
+        # - TODO: other visualizations?
+
+def process_new_data_per_batch() -> List[Dict]:
+    """Enriches topics with new data, returns a list (each element of the list correspond to one batch and is  """
     df_batches = np.array_split(
         st.session_state["remaining_df"], st.session_state["new_data_batches_nb"]
     )
 
     new_topics_over_time = st.session_state["topics_over_time"]
-    tm = TopicMetrics(st.session_state["topic_model"], new_topics_over_time)
-    TEM_map = tm.TEM_map(st.session_state["tw"])
-    TEM_map = tm.identify_signals(TEM_map, TEM_x, TEM_y)
-    TEM_map["batch"] = 0
+
+    results = []
 
     # Need to sort batches by ascending time for correct processing order
     ts_avg = {}
@@ -92,28 +98,42 @@ def process_new_data():
 
             # update model stats
             topics_counter = Counter(topics)
+            words = "" # TODO: update words in the same way as it is done in bertopic (kind of tf-idf)
             topics_over_time_per_batch = [
-                {"Topic": t, "Frequency": f, "Words": "", "Timestamp": ts_avg, "Batch": batch_id}
+                {"Topic": t, "Frequency": f, "Words": words, "Timestamp": ts_avg, "Batch": batch_id}
                 for t, f in topics_counter.items()
             ]
 
             # Combine the batch result with existing info about topics over time
             new_topics_over_time = pd.concat([new_topics_over_time, pd.DataFrame(topics_over_time_per_batch)])
 
-            # New topic metrics (that takes into account the new batch)
-            topic_metrics = TopicMetrics(st.session_state["topic_model"], new_topics_over_time)
-            batch_TEM_map = topic_metrics.TEM_map(st.session_state["tw"])
-            batch_TEM_map = topic_metrics.identify_signals(batch_TEM_map, TEM_x, TEM_y)
-            batch_TEM_map["batch"] = batch_id
-            TEM_map = pd.concat([TEM_map, batch_TEM_map])
+            results.append({"ts_avg": ts_avg, "topics": topics, "probs": probs, "topics_over_time": new_topics_over_time})
+
+    return results
+
+def plot_animated_topic_map(batch_results: List[Dict]):
+    # Topic map based on data before the introduction of new batches
+    tm = TopicMetrics(st.session_state["topic_model"], st.session_state["topics_over_time"])
+    TEM_map = tm.TEM_map(st.session_state["tw"])
+    TEM_map = tm.identify_signals(TEM_map, TEM_x, TEM_y)
+    TEM_map["batch"] = 0
+
+    # Use batch results to compute new data
+    for i, res in enumerate(batch_results):
+        # New topic metrics (that takes into account the new batch)
+        topic_metrics = TopicMetrics(st.session_state["topic_model"], res["topics_over_time"])
+        batch_TEM_map = topic_metrics.TEM_map(st.session_state["tw"])
+        batch_TEM_map = topic_metrics.identify_signals(batch_TEM_map, TEM_x, TEM_y)
+        batch_TEM_map["batch"] = i+1
+        TEM_map = pd.concat([TEM_map, batch_TEM_map])
+
 
     # Plot the resulting map as an animation
     with st.spinner("Plotting topic map..."):
         st.plotly_chart(TopicMetrics.scatterplot_with_annotations(TEM_map, TEM_x, TEM_y, "topic", "topic_description",
-                                                  "Animated Topic Emergence Map (TEM)", TEM_x, TEM_y,
-                                                  animation_frame="batch"
-                                                  ))
-
+                                                                  "Animated Topic Emergence Map (TEM)", TEM_x, TEM_y,
+                                                                  animation_frame="batch"
+                                                                  ))
 
 ###
 # Write page
