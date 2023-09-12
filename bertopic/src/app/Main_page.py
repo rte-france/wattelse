13d07608bc922d2db20e60fd7e93632907cbf3e3
Jérôme Picault <jerome.picault@rte-france.com>
2023-09-12 09:53:54 +0200

    Added reset of "new_topics" when we change data to avoid bugs in the page "simulation of new data"

--- bertopic/src/app/Main_page.py
+++ bertopic/src/app/Main_page.py
@@ -25,6 +25,7 @@ from train_utils import train_BERTopic_wrapper
 def reset_all():
     # TODO: add here all state variables we want to reset when we change the data
     st.session_state.pop("timestamp_range", None)
+    st.session_state.pop("new_topics", None)
     save_widget_state()
 
 
