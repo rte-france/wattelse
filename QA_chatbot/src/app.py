d89853296e9be9e09b47137ef4ecb3be0268c915
Jérôme Picault <jerome.picault@rte-france.com>
2023-09-15 17:20:20 +0200

    Fix bad path

--- QA_chatbot/src/app.py
+++ QA_chatbot/src/app.py
@@ -24,7 +24,7 @@ from utils import (
 )
 
 DATA_DIR = (
-    Path("/data/weak_signals/cache/chatbot_data")
+    Path("/data/weak_signals/chatbot_data")
     if socket.gethostname() == "groesplu0"
     else Path("./data")
 )
