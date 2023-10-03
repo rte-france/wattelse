47a8708a3bd41f24253d6740813f13973f9c0a3f
Guillaume Grosjean <guillaume.grosjean@rte-france.com>
2023-10-03 14:41:50 +0200

    Update MD parsing functions

--- wattelse/common/text_parsers/extract_text_from_MD.py
+++ wattelse/common/text_parsers/extract_text_from_MD.py
@@ -5,58 +5,79 @@ import typer
 from loguru import logger
 
 
-def extract_paragraphs_with_titles(markdown_text):
+def extract_paragraphs_with_levels(markdown_text):
     lines = markdown_text.split("\n")
 
-    current_section = None
-    paragraphs = []
-    paragraph = ""
+    dict_paragraphs = {
+        "level1" : [], # "#" in MD
+        "level2" : [], # "##" in MD
+        "level3" : [], # "###" in MD
+        "paragraph" : [],
+    }
 
-    for line in lines:
-        if line.startswith("#"):  # Section title
-            current_section = line.strip("#").strip()
-        elif line.strip():  # Non-empty line (paragraph content)
+    
+    for i,line in enumerate(lines[:-1]):
+        if line.startswith("#") and line.count("#")==1:  # level 1 section
+            level1 = line.strip("#").strip()
+            level2 = ""
+            level3 = ""
+            paragraph = ""
+        elif line.startswith("##") and line.count("#")==2:  # level 2 section
+            level2 = line.strip("##").strip()
+            level3 = ""
+            paragraph = ""
+        elif line.startswith("###") and line.count("#")==3:  # level 3 section
+            level3 = line.strip("###").strip()
+            paragraph = ""
+        elif line.strip() or lines[i+1].startswith("*"):  # Non-empty line (paragraph content) or next line starts a bullet list
             paragraph += line + "\n"
         elif paragraph:  # Empty line (end of paragraph)
-            #FIXME: à revoir dans le cas où on a une liste avec des bullet points (risque de séparation d'avec le texte qui précède)
-            paragraphs.append(
-                {"section_title": current_section, "text": paragraph.strip()}
-            )
+            dict_paragraphs["level1"].append(level1)
+            dict_paragraphs["level2"].append(level2)
+            dict_paragraphs["level3"].append(level3)
+            dict_paragraphs["paragraph"].append(paragraph)
             paragraph = ""
 
-    if paragraph:  # Handling the last paragraph if it doesn't end with an empty line
-        paragraphs.append(
-            {"section_title": current_section, "text": paragraph.strip()}
-        )
+    # Handle last line
+    paragraph += lines[-1]
+    dict_paragraphs["level1"].append(level1)
+    dict_paragraphs["level2"].append(level2)
+    dict_paragraphs["level3"].append(level3)
+    dict_paragraphs["paragraph"].append(paragraph)
+    paragraph = ""
 
-    return paragraphs
+    return pd.DataFrame(dict_paragraphs)
 
 def extract_text_from_md(markdown_file_path: Path):
     # Read markdown file content
-    with open(markdown_file_path, "r", encoding="utf-8") as file:
+    with open(markdown_file_path, "r", encoding="utf-8-sig") as file:
         markdown_text = file.read()
         # Extract paragraphs with titles
-        paragraphs_with_titles = extract_paragraphs_with_titles(markdown_text)
-        # Keep reference in source file
-        paragraphs_with_titles = [
-            dict(item, **{"file": markdown_file_path.name}) for item in paragraphs_with_titles
-        ]
-        return paragraphs_with_titles
+        paragraphs_with_levels = extract_paragraphs_with_levels(markdown_text)
+        return paragraphs_with_levels
 
 def parse_mds(md_directory: Path, output_file: Path = "./data/output_md.csv") -> Path:
     """Parses a set of .md documents stored in the directory"""
-    # Parsed paragraphs
-    paragraphs = []
+    # DataFrame containing all data
+    df = pd.DataFrame(
+        {
+            "level1" : [], # "#" in MD
+            "level2" : [], # "##" in MD
+            "level3" : [], # "###" in MD
+            "paragraph" : [],
+        }
+        )
 
     # Filter only md files
     for path in Path(md_directory).iterdir():
         if path.is_file() and path.suffix == ".md":
             logger.info(f"Parsing file: {path}")
-            paragraphs += extract_text_from_md(path)
-
-    df = pd.DataFrame(paragraphs).fillna("")
+            df = pd.concat([df, extract_text_from_md(path)])
+    
+    df = df.fillna("")
     # Combine columns to enrich the text
-    df["processed_text"] = "Fichier: " + df.file + "\nTitre: " + df.section_title + "\n" + df.text
+    df["processed_text"] = df.level1 + " | " + df.level2 + " | " + df.level3 + " | " + df.paragraph
+    df["processed_text"] = df["processed_text"].str.replace("|  |", "|")
 
     df.to_csv(output_file)
 
