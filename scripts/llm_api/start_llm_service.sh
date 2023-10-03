33623539c45d2c3aaf5f5694e0dda431925b0def
Jérôme Picault <jerome.picault@rte-france.com>
2023-10-03 16:22:12 +0200

    Variable for model

--- scripts/llm_api/start_llm_service.sh
+++ scripts/llm_api/start_llm_service.sh
@@ -2,6 +2,9 @@
 # This script has to be run on a GPU server!
 # use the option "--load-8bit" to launch it in 8-bit mode
 
+MODEL=bofenghuang/vigogne-2-7b-instruct
+#MODEL=bofenghuang/vigogne-2-13b-instruct
+
 HOST=localhost
 PORT_API=8888
 PORT_CONTROLLER=21001
@@ -13,11 +16,11 @@ python3 -m fastchat.serve.controller --host $HOST --port $PORT_CONTROLLER &
 # launch worker
 if [ -z "$1" ]; then
   # no args provided
-  CUDA_VISIBLE_DEVICES=2,1 python3 -m fastchat.serve.model_worker --host $HOST --port $PORT_WORKER --worker-address http://$HOST:$PORT_WORKER --controller-address http://$HOST:$PORT_CONTROLLER --model-path bofenghuang/vigogne-2-7b-instruct --num-gpus 2&
+  CUDA_VISIBLE_DEVICES=2,1 python3 -m fastchat.serve.model_worker --host $HOST --port $PORT_WORKER --worker-address http://$HOST:$PORT_WORKER --controller-address http://$HOST:$PORT_CONTROLLER --model-path $MODEL --num-gpus 2&
 else
   # we assume any args means '--load-8-bit' (NB. it seems that if we do not change the num-gpus values, the parameter is not taken into account
   echo "Starting service in 8-bit mode"
-  CUDA_VISIBLE_DEVICES=2,1 python3 -m fastchat.serve.model_worker --host $HOST --port $PORT_WORKER --worker-address http://$HOST:$PORT_WORKER --controller-address http://$HOST:$PORT_CONTROLLER --model-path bofenghuang/vigogne-2-7b-instruct --num-gpus 1 --load-8bit&
+  CUDA_VISIBLE_DEVICES=2,1 python3 -m fastchat.serve.model_worker --host $HOST --port $PORT_WORKER --worker-address http://$HOST:$PORT_WORKER --controller-address http://$HOST:$PORT_CONTROLLER --model-path $MODEL --num-gpus 1 --load-8bit&
 fi
 
 # launch API server
