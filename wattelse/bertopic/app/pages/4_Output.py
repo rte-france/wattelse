0e6c6f209b1e0a735525f1c14ed5901a1cfcfbec
Guillaume Grosjean <guillaume.grosjean@rte-france.com>
2023-10-26 15:42:40 +0200

    Fix newsletter generation in streamlit app

--- wattelse/bertopic/app/pages/4_Output.py
+++ wattelse/bertopic/app/pages/4_Output.py
@@ -2,6 +2,12 @@ import markdown
 import streamlit as st
 
 from wattelse.bertopic.ouput_features import generate_newsletter
+from wattelse.bertopic.utils import load_data
+from wattelse.bertopic.app.state_utils import restore_widget_state
+from wattelse.bertopic.utils import DATA_DIR
+from wattelse.summary.chatgpt_summarizer import GPTSummarizer
+
+restore_widget_state()
 
 # Stop app if no topic is selected
 if "topic_model" not in st.session_state.keys():
@@ -12,7 +18,7 @@ if "topic_model" not in st.session_state.keys():
 if st.button("Generate newsletter"):
     md = generate_newsletter(
         st.session_state["topic_model"],
-        st.session_state["raw_df"],#load_data(st.session_state["data_name"]),
+        load_data(f"{DATA_DIR}/{st.session_state['data_name']}"),
         st.session_state["topics"],
         df_split=st.session_state["timefiltered_df"],
     )
