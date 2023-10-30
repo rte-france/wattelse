023fbac4593a53aff4f85e1dc9721a76348466d6
Jérôme Picault <jerome.picault@rte-france.com>
2023-10-30 17:23:58 +0100

    Added error catching in case the API does not return a result

--- wattelse/summary/chatgpt_summarizer.py
+++ wattelse/summary/chatgpt_summarizer.py
@@ -3,6 +3,7 @@ import openai
 import tiktoken
 from pathlib import Path
 from loguru import logger
+from openai import APIError
 
 from wattelse.summary.summarizer import Summarizer
 from wattelse.summary.prompts import BASE_PROMPT_SUMMARY
@@ -29,17 +30,21 @@ class GPTSummarizer(Summarizer):
         return num_tokens
 
     def generate_summary(self, article_text, max_summary_length_ratio=0.5, max_article_length=2000) -> str:
-        # Limit input length :
-        encoded_article_text = self.encoding.encode(article_text)
-        if len(encoded_article_text) > max_article_length:
-            encoded_article_text = encoded_article_text[0:max_article_length]
-            article_text = self.encoding.decode(encoded_article_text)
-        # Create answer object
-        answer = openai.ChatCompletion.create(
-            model=MODEL,
-            messages=[{"role": "user", "content": BASE_PROMPT_SUMMARY.format(text=article_text)}],
-            max_tokens=round(self.num_tokens_from_string(article_text)*max_summary_length_ratio),
-            temperature=TEMPERATURE,
-        )
-        logger.debug(f"API returned: {answer}")
-        return answer.choices[0].message.content
+        try:
+            # Limit input length :
+            encoded_article_text = self.encoding.encode(article_text)
+            if len(encoded_article_text) > max_article_length:
+                encoded_article_text = encoded_article_text[0:max_article_length]
+                article_text = self.encoding.decode(encoded_article_text)
+            # Create answer object
+            answer = openai.ChatCompletion.create(
+                model=MODEL,
+                messages=[{"role": "user", "content": BASE_PROMPT_SUMMARY.format(text=article_text)}],
+                max_tokens=round(self.num_tokens_from_string(article_text)*max_summary_length_ratio),
+                temperature=TEMPERATURE,
+            )
+            logger.debug(f"API returned: {answer}")
+            return answer.choices[0].message.content
+        except APIError as e:
+            return f"OpenAI API error : {e}"
+            logger.error(f"OpenAI API error : {e}")
