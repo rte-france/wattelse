d0ec97f8ef359d8cb55ec41ce9e371cebf91a45a
Jérôme Picault <jerome.picault@rte-france.com>
2023-10-31 14:28:17 +0100

    Bug fix: openai api_base is changed if the config is loaded outside of the constructor

--- wattelse/llm/vllm_api.py
+++ wattelse/llm/vllm_api.py
@@ -6,41 +6,57 @@ import openai
 from loguru import logger
 from transformers import AutoTokenizer
 
-config = configparser.ConfigParser()
-config.read(Path(__file__).parent.parent / "config" / "llm_api.cfg")
-openai.api_key = config.get("LLM_API_CONFIG", "openai_key")
-openai.api_base = config.get("LLM_API_CONFIG", "openai_url")
-
 
 def get_api_model_name():
-        """
-        Return currently loaded model name in vLLM.
-        """
-        try :
-            # First model
-            models = openai.Model.list()
-            return models["data"][0]["id"]
-        except Exception as e:
-            return None
+    """
+    Return currently loaded model name in vLLM.
+    """
+    try:
+        # First model
+        models = openai.Model.list()
+        return models["data"][0]["id"]
+    except Exception as e:
+        return None
 
 
-class vLLM_API():
-
+class vLLM_API:
     def __init__(self):
+        config = configparser.ConfigParser()
+        config.read(Path(__file__).parent.parent / "config" / "llm_api.cfg")
+        openai.api_key = config.get("LLM_API_CONFIG", "openai_key")
+        openai.api_base = config.get("LLM_API_CONFIG", "openai_url")
+
         self.model_name = get_api_model_name()
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, padding_side="right", use_fast=False) if self.model_name else None
+        self.tokenizer = (
+            AutoTokenizer.from_pretrained(
+                self.model_name, padding_side="right", use_fast=False
+            )
+            if self.model_name
+            else None
+        )
 
     def generate_llm_specific_prompt(self, prompt) -> str:
         """
         Takes a prompt as input and returns the prompt in the specific LLM format.
         """
-        chat = [{
+        chat = [
+            {
                 "role": "user",
                 "content": prompt,
-                }]
-        return self.tokenizer.apply_chat_template(chat, add_generation_prompt=True, tokenize = False)
+            }
+        ]
+        return self.tokenizer.apply_chat_template(
+            chat, add_generation_prompt=True, tokenize=False
+        )
 
-    def generate(self, prompt, temperature=0.1, max_tokens=512, transform_prompt=True, stream=False) -> str:
+    def generate(
+        self,
+        prompt,
+        temperature=0.1,
+        max_tokens=512,
+        transform_prompt=True,
+        stream=False,
+    ) -> str:
         """Uses the remote model (API) to generate the answer.
 
         Args:
@@ -51,7 +67,7 @@ class vLLM_API():
             stream (bool, optional): Whether to stream output. Defaults to False.
 
         Returns:
-            str: Returns : 
+            str: Returns :
                     - full text once generated if stream=False
                     - Completion object if stream=True
         """
