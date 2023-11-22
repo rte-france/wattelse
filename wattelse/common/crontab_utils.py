f4af8ccfcd198fc83f7208d182f5c4bcc262ffa8
Jérôme Picault <jerome.picault@rte-france.com>
2023-11-22 23:30:22 +0100

    Bug fix: crontab command updated for non superusers

--- wattelse/common/crontab_utils.py
+++ wattelse/common/crontab_utils.py
@@ -7,7 +7,7 @@ def add_job_to_crontab(schedule, command, env_vars=""):
     logger.info(f"Adding to crontab: {schedule} {command}")
     # Create crontab, add command
     cmd = (
-        f'(crontab -u $(whoami) -l; echo "{schedule} umask 002; {env_vars} {command}" ) | crontab -u $(whoami) -'
+        f'(crontab -l; echo "{schedule} umask 002; {env_vars} {command}" ) | crontab -'
     )
     returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
     logger.info(f"Crontab updated with status {returned_value}")
