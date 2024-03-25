e6e7623f0e63e9cb27334140bfca2867e230440b
Jérôme Picault <jerome.picault@rte-france.com>
2024-03-25 15:25:45 +0100

    Changed default cache location for GPU DSVD servers

--- wattelse/common/__init__.py
+++ wattelse/common/__init__.py
@@ -5,11 +5,11 @@ TEXT_COLUMN = "text"
 FILENAME_COLUMN = "filename"
 SEED = 666
 GPU_SERVERS = ["groesplu0", "GROESSLAO01"]
-GPU_DSVD = ["pf9sodsia001"]
+GPU_DSVD = ["pf9sodsia001", "pf9sodsia002", "pf9sodsia003"]
 BASE_DATA_DIR = (
     Path("/data/weak_signals/data/")
     if socket.gethostname() in GPU_SERVERS
-    else Path("/scratch/weak_signals/data/")
+    else Path("/scratch/nlp/data/")
     if socket.gethostname() in GPU_DSVD
     else Path(__file__).parent.parent.parent / "data"
 )
