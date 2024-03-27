71df4333a9bb492c6f2d549322fce23235772621
dejaegherjer <jerome.dejaegher@rte-france.com>
2024-03-27 17:37:44 +0100

    changement du summary_model par defaut.

--- wattelse/summary/app.py
+++ wattelse/summary/app.py
@@ -4,10 +4,11 @@ from abstractive_summarizer import AbstractiveSummarizer
 from extractive_summarizer import ExtractiveSummarizer
 from wattelse.summary.chatgpt_summarizer import GPTSummarizer
 
-MODEL_LIST = [ "chat_gpt",
-               "origami_summarizer",
-               "mrm8488/camembert2camembert_shared-finetuned-french-summarization",
-               "csebuetnlp/mT5_multilingual_XLSum",
+MODEL_LIST = [ 
+    "origami_summarizer",
+    "mrm8488/camembert2camembert_shared-finetuned-french-summarization",
+    "csebuetnlp/mT5_multilingual_XLSum",
+    "chat_gpt",
                ]
 
 DEFAULT_TEXT = """RTE, sigle du Réseau de transport d'électricité, est le gestionnaire de réseau de transport français responsable du réseau public de transport d'électricité haute tension en France métropolitaine (la Corse n'est pas gérée par RTE). Sa mission fondamentale est d’assurer à tous ses clients l’accès à une alimentation électrique économique, sûre et propre. RTE connecte ses clients par une infrastructure adaptée et leur fournit tous les outils et services qui leur permettent d’en tirer parti pour répondre à leurs besoins. À cet effet, RTE exploite, maintient et développe le réseau à haute et très haute tension. Il est le garant du bon fonctionnement et de la sûreté du système électrique. RTE achemine l’électricité entre les fournisseurs d’électricité (français et européens) et les consommateurs, qu’ils soient distributeurs d’électricité ou industriels directement raccordés au réseau de transport. Plus de 105 000 km de lignes comprises entre 45 000 et 400 000 volts et 50 lignes transfrontalières connectent le réseau français à 33 pays européens, offrant ainsi des opportunités d’échanges d’électricité essentiels pour l’optimisation économique du système électrique.
@@ -38,6 +39,10 @@ def get_summarizer(summary_model):
 def app():
 
     st.title("Summarizer (abstractive & extractive")
+
+    st.write(
+        "    Le modèle chat_gpt n'est pas un modèle privé. \nMerci de l'utiliser EXCLUSIVEMENT sur des données publics."
+        )
 
     summary_model = st.selectbox("summary model", MODEL_LIST, key="summary_model")
     summary_ratio = st.number_input("summary ratio", min_value=1, max_value=100, value=20, key="summary_ratio")/100
