2dfead7447d058c4555b917421190345d8cd19bf
Jérôme Picault <jerome.picault@rte-france.com>
2024-03-27 16:31:52 +0100

    Bad import

--- wattelse/chatbot/frontend/django_chatbot/settings.py
+++ wattelse/chatbot/frontend/django_chatbot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from wattelse.chatbot import BASE_DATA_DIR
+from wattelse.chatbot.backend import BASE_DATA_DIR
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
