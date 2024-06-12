df7e2008f6f6bf301447cf01f70bba7982298696
Jerome Picault <jerome.picault@rte-france.com>
2024-06-12 16:20:18 +0200

    WIP

--- exploration/curebot/veille_analyse.py
+++ exploration/curebot/veille_analyse.py
@@ -2,11 +2,14 @@
 #  See AUTHORS.txt
 #  SPDX-License-Identifier: MPL-2.0
 #  This file is part of Wattelse, a NLP application suite.
+from io import BytesIO
 from pathlib import Path
-from tempfile import TemporaryDirectory
+from tempfile import TemporaryDirectory, NamedTemporaryFile
+from typing import List
 
 import pandas as pd
 import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from wattelse.bertopic.newsletter_features import generate_newsletter
 from wattelse.bertopic.train import train_BERTopic
@@ -16,25 +19,29 @@ from wattelse.summary import GPTSummarizer
 
 COLUMN_URL = "url"
 MIN_TEXT_LENGTH = 150
+EMBEDDING_MODEL_NAME = "dangvantuan/sentence-camembert-large"
 
-
-def parse_data(files) -> pd.DataFrame:
+def parse_data(files: List[UploadedFile]) -> pd.DataFrame:
     """Read a list of excel files and return a single dataframe containing the data"""
     dataframes = []
 
     with TemporaryDirectory() as tmpdir:
 
         for f in files:
-            if f is not None:
-                print(f)
-                st.info(f"Analyse des articles de {f.name}")
-                provider = CurebotProvider(f.name)
-                articles = provider.get_articles()
-                provider.store_articles(articles, Path(tmpdir) / (f.name + ".jsonl"))
-                df = load_data(tmpdir + "/" + f.name + ".jsonl").sort_values(by=TIMESTAMP_COLUMN,
-                                                                             ascending=False).reset_index(
-                    drop=True).reset_index()
-                dataframes.append(df)
+            with NamedTemporaryFile(mode="wb") as tmp_file:
+                # Copy data from BytesIO to the temporary file
+                tmp_file.write(f.getvalue())
+                if tmp_file is not None:
+                    st.info(f"Analyse des articles de: {f.name}")
+                    provider = CurebotProvider(Path(tmp_file.name))
+                    articles = provider.get_articles()
+                    #articles_path = Path(tmpdir) / (f.name + ".jsonl")
+                    articles_path = Path("/tmp") / (f.name + ".jsonl")
+                    provider.store_articles(articles, articles_path)
+                    df = load_data(articles_path.absolute().as_posix()).sort_values(by=TIMESTAMP_COLUMN,
+                                                                                 ascending=False).reset_index(
+                        drop=True).reset_index()
+                    dataframes.append(df)
 
         # Concat all dataframes
         df_concat = pd.concat(dataframes, ignore_index=True)
@@ -65,6 +72,7 @@ def train_model():
     st.session_state["topic_model"], st.session_state["topics"], _ = train_BERTopic(
         full_dataset=st.session_state["df_split"],
         indices=None,
+        embedding_model_name=EMBEDDING_MODEL_NAME,
         use_cache=False,
     )
     st.success("Création de topics... OK")
