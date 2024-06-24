88e90bd34dfdd4f9589829b72612d9f66f7833ee
Jérôme Picault <jerome.picault@rte-france.com>
2024-06-24 18:18:57 +0200

    [Newsletter] Dedicated API_KEY for veille & analyse

--- exploration/curebot/start_newsletter_generator.sh
+++ exploration/curebot/start_newsletter_generator.sh
@@ -3,4 +3,4 @@
 #  SPDX-License-Identifier: MPL-2.0
 #  This file is part of Wattelse, a NLP application suite.
 
-CUDA_VISIBLE_DEVICES=0 streamlit run  --theme.primaryColor royalblue --server.port 8686 veille_analyse.py
+OPENAI_API_KEY=$OPENAI_API_KEY_VEILLE CUDA_VISIBLE_DEVICES=0 streamlit run  --theme.primaryColor royalblue --server.port 8686 veille_analyse.py
