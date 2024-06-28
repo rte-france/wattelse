38488e4459d6ed11b7169f4c6f7469a42165eb25
yhaslan <yagmuraslan04@gmail.com>
2024-06-28 11:29:00 +0200

    modified the vector_database script to use the custom chroma class

--- wattelse/chatbot/backend/vector_database.py
+++ wattelse/chatbot/backend/vector_database.py
@@ -15,7 +15,7 @@ from typing import Optional, List, Dict
 
 import chromadb
 from chromadb import Embeddings
-from langchain_community.vectorstores.chroma import Chroma
+from wattelse.chatbot.backend.custom_chroma import CustomChroma as Chroma
 from langchain_core.documents import Document
 from loguru import logger
 
