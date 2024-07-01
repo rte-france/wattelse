817fb6556e4bf8159990bf73e2a66b90661f5402
Guillaume Grosjean <guillaume.grosjean@rte-france.com>
2024-07-01 11:14:14 +0200

    [RAG] Add password authentication for dashboard

--- wattelse/chatbot/frontend/dashboard.py
+++ wattelse/chatbot/frontend/dashboard.py
@@ -2,6 +2,7 @@
 #  See AUTHORS.txt
 #  SPDX-License-Identifier: MPL-2.0
 #  This file is part of Wattelse, a NLP application suite.
+import hmac
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -274,14 +275,43 @@ def display_feedback_rates():
                     "",
                 )
             
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+            
 
 
 def main():
     # Wide layout
     st.set_page_config(page_title="Wattelse dashboard", layout="wide")
 
+    # Title
     st.title("Wattelse dashboard")
-    # load data
+
+    # Password
+    if not check_password():
+        st.stop()  # Do not continue if check_password is not True.
+
+    # Load data
     st.session_state["full_data"] = get_db_data(DB_PATH)
 
     if side_bar():
