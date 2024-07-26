d1025aa0b94321a896fd368e7005a8b551064d35
Guillaume Grosjean <guillaume.grosjean@rte-france.com>
2024-07-26 17:24:01 +0200

    [DCR] Save commit

--- exploration/raccordement/app.py
+++ exploration/raccordement/app.py
@@ -2,12 +2,13 @@ import os
 import tempfile
 import yaml
 from pathlib import Path
+from docxtpl import DocxTemplate
 
 import streamlit as st
 
 from wattelse.api.rag_orchestrator.rag_client import RAGOrchestratorClient
 
-
+st.set_page_config(layout="wide")
 
 PATH_TO_EXAMPLE_D1_DIR = Path(__file__).parent / "example_D1"
 
@@ -15,60 +16,101 @@ RAG_CLIENT = RAGOrchestratorClient()
 SESSION_NAME = "test_raccordement"
 RAG_CLIENT.create_session(SESSION_NAME)
 
-with open("information.yaml", 'r') as f:
-	INFO_DICT = yaml.safe_load(f)
+st.logo("./RTE_logo.svg")
 
-# st.title("Automatisation note DCR")
+with open("RAG_information.yaml", "r") as f:
+    INFO_DICT = yaml.safe_load(f)
 
-tab1, tab2, tab3, tab4 = st.tabs(["Accueil", "Fiche D1", "RAG", "Note DCR"])
+def download_study(context: dict):
+    doc = DocxTemplate("./Trame-DTR_streamlit.docx")
+    doc.render(context)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        doc.save(temp_dir + "etude_test.docx")
+        with open(temp_dir + "etude_test.docx", "rb") as f:
+            st.download_button("Télécharger Word", f, file_name="etude_test.docx")
+
+
+# Sidebar
+with st.sidebar:
+    st.title("POC synthèse DCR")
+    st.write("---")
+    if st.button("Générer Word"):
+        download_study(st.session_state)
+
+# Main page
+tab1, tab2 = st.tabs(["Fiche D1", "RAG"])
 
 with tab1:
-    st.write(
-        "Cette application a pour objectif de démontrer la faisaibilité "
-        "d'un processus d'automatisation de la rédaction des notes "
-        "d'étude exploratoire DCR envoyées aux clients à partir d'une "
-        "fiche D1 et de la note d'étude SER associée."
-    )
-
-with tab2:
     st.title("Informations fiche D1")
-    with st.expander("Formulaire D1"):
+    col1, col2 = st.columns(2)
+    with col1:
         with st.form("D1"):
-            # Study ID
-            st.write("**Identifiant de l'étude**")
-            st.text_input("Numéro", placeholder="AA-XXX", key="study_id")
+            st.text_input("ID étude", placeholder="AA-XXX", key="D1_id_etude")
+            st.selectbox(
+                "Civilité contact", ["Madame", "Monsieur"], key="D1_civilité_contact"
+            )
+            st.text_input(
+                "Prénom Nom contact",
+                placeholder="Prénom Nom",
+                key="D1_prenom_nom_contact",
+            )
+            st.text_input(
+                "Adresse email contact", placeholder="xxx@yyy", key="D1_email_contact"
+            )
+            st.text_input("Nom société", placeholder="xxx", key="D1_nom_societe")
 
-            # Client information
-            st.write("**Informations client**")
-            st.selectbox("Civilité contact", ["Madame", "Monsieur"], key="client_civil_status")
-            st.text_input("Nom contact", key="client_name")
-            st.text_input("Adresse email contact", key="client_email_address")
-            st.text_input("Nom société", key="client_company_name")
-
-            # Study information
-            st.write("**Informations sur l'étude**")
-            st.date_input("Date de la demande", format="DD/MM/YYYY", key="request_date")
-            st.text_input("Type de filière", key="connection_type")
-            st.text_input("Puissance installée en injection", key="generation_capacity_installed")
-            st.text_input("Puissance de raccordement en injection", key="generation_connection_power")
-            st.text_input("Puissance de raccordement en soutirage", key="load_connection_power")
-            st.date_input("Date de mise en service souhaitée", format="DD/MM/YYYY", key="desired_commissioning_date")
-
-            # Geographical information
-            st.write("**Informations géographiques du point de raccordement**")
-            st.number_input("Numéro de département", min_value=1, max_value=100, step=1, key="department_number")
-            st.text_input("Nom du département", key="departement_name")
-            st.text_input("Nom de la commune", key="commune_name")
+            st.text_input(
+                "Date de la demande", placeholder="DD/MM/YYYY", key="D1_date_demande"
+            )
+            st.text_input("Type de filière", placeholder="xxx", key="D1_type_filiere")
+            st.number_input(
+                "Puissance de raccordement en injection (MW)",
+                min_value=1,
+                max_value=100,
+                step=1,
+                value=10,
+                key="D1_puissance_injection",
+            )
+            st.number_input(
+                "Puissance de raccordement en soutirage (MW)",
+                min_value=1,
+                max_value=100,
+                step=1,
+                value=10,
+                key="D1_puissance_soutirage",
+            )
+            st.text_input(
+                "Date de mise en service souhaitée",
+                placeholder="DD/MM/YYYY",
+                key="D1_mise_service",
+            )
+            st.number_input(
+                "Numéro de département",
+                min_value=1,
+                max_value=100,
+                step=1,
+                key="D1_numero_departement_raccordement",
+            )
+            st.text_input(
+                "Nom de la commune", placeholder="xxx", key="D1_commune_raccordement"
+            )
 
             # Form submit button
             st.form_submit_button("Valider")
 
+    def load_example_d1(file: Path):
+        with open(file, "r") as f:
+            d1_dict = yaml.safe_load(f)
+            st.session_state.update(d1_dict)
+
+    with col2:
         st.subheader("Exemples de formulaire pré-remplis :")
+        for file in PATH_TO_EXAMPLE_D1_DIR.glob("*.yaml"):
+            if not file.name.startswith("_"):
+                st.button(file.name, on_click=load_example_d1, args=(file,))
+            
 
-    for file in PATH_TO_EXAMPLE_D1_DIR.glob("*.yaml"):
-        st.button(file.name)
-
-with tab3:
+with tab2:
     # Title
     st.title("RAG étude SER")
 
@@ -81,26 +123,34 @@ with tab3:
     else:
         with tempfile.TemporaryDirectory() as temp_dir:
             temp_file_path = os.path.join(temp_dir, uploaded_file.name)
-            with open(temp_file_path, 'wb') as temp_file:
+            with open(temp_file_path, "wb") as temp_file:
                 temp_file.write(uploaded_file.getbuffer())
             print(temp_file_path)
             RAG_CLIENT.upload_files(SESSION_NAME, [temp_file_path])
 
     # Function
     def unit_RAG(label: str, prompt: str = None):
-        with st.expander(label):
-            st.text_area("Prompt", value=prompt, key="prompt_"+label)
-            if st.button("Lancer", key="button_"+label):
-                response = RAG_CLIENT.query_rag(SESSION_NAME, st.session_state["prompt_"+label], selected_files=[uploaded_file.name])
-                st.write("Réponse :")
-                st.write("**"+response["answer"]+"**")
-                st.session_state["answer_"+label] = response["answer"]
-
+        st.subheader(label)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.text_area("Prompt", value=prompt, key="prompt_" + label)
+        if st.button("Lancer", key="button_" + label):
+            response = RAG_CLIENT.query_rag(
+                SESSION_NAME,
+                st.session_state["prompt_" + label],
+                selected_files=[uploaded_file.name],
+            )
+            # st.write("Réponse :")
+            # st.write("**"+response["answer"]+"**")
+            st.session_state["answer_" + label] = response["answer"]
+        with col2:
+            st.text_area(
+                "Réponse",
+                value=st.session_state.get("answer_" + label),
+                key="answer_" + label,
+            )
 
     # Main connection solution
-    st.header("Solution de raccordement principale")
 
     for key, value in INFO_DICT.items():
-        st.subheader(key)
-        for info in value:
-            unit_RAG(info["name"], prompt=info.get("prompt"))
+        unit_RAG(value["name"], prompt=value.get("prompt"))
