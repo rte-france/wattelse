d89af3a4f736f753bad613e1028d8beec02d644d
Jérôme Picault <jerome.picault@rte-france.com>
2024-07-30 13:48:24 +0200

    Fixed log paths

--- wattelse/common/__init__.py
+++ wattelse/common/__init__.py
@@ -17,8 +17,8 @@ BASE_OUTPUT_PATH = BASE_PATH / "output"
 BASE_CACHE_PATH = BASE_PATH / "cache"
 MODEL_BASE_PATH = BASE_PATH / "models"
 FEED_BASE_PATH = BASE_DATA_PATH / "bertopic" / "feeds"
-BERTOPIC_LOG_PATH = BASE_PATH / "bertopic" / "log"
-WATTELSE_LOG_PATH = BASE_PATH / "wattelse" / "log"
+BERTOPIC_LOG_PATH = BASE_PATH / "logs" / "bertopic"
+WATTELSE_LOG_PATH = BASE_PATH / "logs" / "wattelse"
 
 BERTOPIC_LOG_PATH.mkdir(parents=True, exist_ok=True)
 WATTELSE_LOG_PATH.mkdir(parents=True, exist_ok=True)
