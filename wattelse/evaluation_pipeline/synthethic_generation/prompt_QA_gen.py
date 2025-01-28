053bc09849baffa9bf2e001367c94b92ef4e6cf7
nadernlp <nader.narcisse@rte-france.com>
2025-01-28 13:13:41 +0100

    [QA-Generation] Removed the evaluation prompt for Synthetic QA

--- wattelse/evaluation_pipeline/synthethic_generation/prompt_QA_gen.py
+++ wattelse/evaluation_pipeline/synthethic_generation/prompt_QA_gen.py
@@ -1,5 +1,5 @@
 # QA generation prompt template
-# FIXME : redefine nuances + use better model
+# FIXME : redefine nuances + use bigger model
 QA_GENERATION_PROMPT = """
 Votre tâche est d'écrire une question factuelle et sa réponse en fonction d'un contexte donné.
 Vous devez créer trois types de questions :
@@ -29,20 +29,36 @@ Contexte : {context}\n
 Sortie:::
 """
 
-# Groundedness (Pertinence contextuelle) : La question doit pouvoir être répondue à partir du contexte donné 
-# pour éviter tout type d'hallucinations.
-QUESTION_GROUNDEDNESS_CRITIQUE_PROMPT = """
-Avec ce contexte, est-ce que je peux répondre à cette question ?
+QA_GENERATION_PROMPT_DEEPSEEK = """
+Vous êtes un employé de l'entreprise et vous avez des questions concernant la politique de voyage de celle-ci. 
+Votre tâche est de rédiger une question factuelle ainsi que sa réponse, en fonction d'un contexte donné.
+Les questions doivent être du type qu'un employé poserait pour obtenir des informations liées à la politique de voyage de l'entreprise.
 
-Réponse:::
-Évaluation : (votre raisonnement pour la note, sous forme de texte)
-Note totale : (votre note, sous forme de nombre entre 1 et 5)
+Vous devez créer trois types de questions :
 
-Vous DEVEZ fournir des valeurs pour 'Évaluation :' et 'Note totale :' dans votre réponse.
+1. **Simple** : une question factuelle concise qui peut être répondue directement avec une information simple du contexte.
+2. **Raisonnement** : une question qui nécessite un raisonnement ou une déduction à partir des éléments du contexte.
+3. **Multi-contexte** : une question qui intègre plusieurs éléments ou informations du contexte pour formuler la réponse.
 
-Voici maintenant la question et le contexte.
+Votre question factuelle doit être répondue par des éléments d'information factuelle provenant du contexte. 
+Cela signifie que votre question factuelle NE DOIT PAS mentionner quelque chose comme "selon le passage" ou "Dans le contexte".
 
-Question : {question}\n
+NE FOURNIS PAS DE RAISONNEMENT ou d'explication dans les réponses. Les réponses doivent se limiter aux informations directement issues du contexte.
+
+Fournissez votre réponse comme suit :
+
+Sortie:::
+Question simple : (votre question simple)
+Réponse simple : (votre réponse doit être claire, synthétique, et formulée sous forme de phrase complète à la question simple)
+
+Question de raisonnement : (votre question de raisonnement)
+Réponse de raisonnement : (votre réponse doit être claire, synthétique, et formulée sous forme de phrase complète à la question de raisonnement)
+
+Question multi-contexte : (votre question multi-contexte)
+Réponse multi-contexte : (votre réponse doit être claire, synthétique, et formulée sous forme de phrase complète à la question multi-contexte)
+
+Voici maintenant le contexte.
+
 Contexte : {context}\n
-Réponse:::
+Sortie:::
 """
