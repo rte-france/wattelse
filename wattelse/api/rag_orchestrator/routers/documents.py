56ac94cea55d7dcc800bacef50429eea04ae3cc2
Guillaume Grosjean <guillaume.grosjean@rte-france.com>
2025-02-27 17:37:09 +0100

    [RAG] Add missing endpoint

--- wattelse/api/rag_orchestrator/routers/documents.py
+++ wattelse/api/rag_orchestrator/routers/documents.py
@@ -1,11 +1,12 @@
 import json
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, File, HTTPException, UploadFile, logger
 
 from wattelse.api.rag_orchestrator import (
     ENDPOINT_CLEAR_COLLECTION,
     ENDPOINT_DOWNLOAD,
     ENDPOINT_LIST_AVAILABLE_DOCS,
     ENDPOINT_REMOVE_DOCS,
+    ENDPOINT_UPLOAD_DOCS,
 )
 from starlette.responses import FileResponse
 
@@ -15,6 +16,31 @@ from wattelse.api.rag_orchestrator.utils import require_session
 
 
 router = APIRouter()
+
+
+@router.post(ENDPOINT_UPLOAD_DOCS + "/{group_id}")
+@require_session
+def upload(group_id: str, files: list[UploadFile] = File(...)):
+    """Upload a list of documents into a document collection"""
+    # Get current document collection
+    collection = RAG_SESSIONS[group_id].document_collection
+    collection_name = collection.collection_name
+
+    for file in files:
+        logger.debug(f"[Group: {group_id}] Uploading file: {file.filename}...")
+        # Check if the file is already in the document collection
+        if collection.is_present(file.filename):
+            logger.warning(
+                f"[Group: {group_id}] File {file.filename} already present in the collection {collection_name}, "
+                f"skipping indexing and chunking"
+            )
+            continue
+
+        RAG_SESSIONS[group_id].add_file_to_collection(file.filename, file.file)
+
+    return {
+        "message": f"[Group: {group_id}] Successfully uploaded {[file.filename for file in files]}"
+    }
 
 
 @router.post(ENDPOINT_REMOVE_DOCS + "/{group_id}")
