ffc1c6accd91f3b5d52d2c531fea969ba6204114
Jérôme Picault <jerome.picault@rte-france.com>
2025-04-18 14:46:51 +0200

    Improved README

--- wattelse/web_app/stop.py
+++ wattelse/web_app/stop.py
@@ -8,4 +8,4 @@ import os
 from wattelse.web_app.config.settings import CONFIG
 
 # Stop processes associated to API port
-os.system(f"kill $(lsof -t -i:{CONFIG.port})")
+os.system(f"kill $(lsof -t -i:{CONFIG.django.port})")
